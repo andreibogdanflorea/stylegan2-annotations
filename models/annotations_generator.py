72cc2bfda885c1344b6f256d560ed444bd4185f0
Andrei-Bogdan FLOREA <andrei.florea0405@stud.acs.upb.ro>
2023-03-25 21:02:33 +0200

    Changed to 512x512 model

--- models/annotations_generator.py
+++ models/annotations_generator.py
@@ -45,18 +45,18 @@ class AnnotationsGAN(nn.Module):
         self.low_feature_channels = 128
         self.conv_low_features = nn.Conv2d(5 * 512, self.low_feature_channels, kernel_size=1, bias=False)
         
-        self.mid_feature_size = 256
+        self.mid_feature_size = 128
         self.mid_feature_channels = 64
-        self.conv_mid_features = nn.Conv2d(384, self.mid_feature_channels, kernel_size=1, bias=False)
+        self.conv_mid_features = nn.Conv2d(256, self.mid_feature_channels, kernel_size=1, bias=False)
 
         self.low_mid_mix = SegBlock(
             self.low_feature_channels + self.mid_feature_channels,
             self.low_feature_channels + self.mid_feature_channels
         )
 
-        self.high_feature_size = 1024
+        self.high_feature_size = 512
         self.high_feature_channels = 32
-        self.conv_high_features = nn.Conv2d(96, self.high_feature_channels, kernel_size=1, bias=False)
+        self.conv_high_features = nn.Conv2d(192, self.high_feature_channels, kernel_size=1, bias=False)
 
         self.low_mid_high_mix = SegBlock(
             self.low_feature_channels + self.mid_feature_channels + self.high_feature_channels,
@@ -78,6 +78,8 @@ class AnnotationsGAN(nn.Module):
             return_features=True
         )
 
+        print(list(map(lambda x: x.shape, features)))
+
         low_features = [
             F.interpolate(features[0], size=self.low_feature_size, mode='bilinear'),
             F.interpolate(features[1], size=self.low_feature_size, mode='bilinear'),
@@ -90,8 +92,7 @@ class AnnotationsGAN(nn.Module):
         low_features = F.interpolate(low_features, size=self.mid_feature_size, mode='bilinear')
 
         mid_features = [
-            F.interpolate(features[5], size=self.mid_feature_size, mode='bilinear'),
-            features[6],
+            features[5],
         ]
         mid_features = torch.cat(mid_features, dim=1)
         mid_features = self.conv_mid_features(mid_features)
@@ -101,8 +102,8 @@ class AnnotationsGAN(nn.Module):
         low_mid_features = F.interpolate(low_mid_features, size=self.high_feature_size, mode='bilinear')
 
         high_features = [
-            F.interpolate(features[7], size=self.high_feature_size, mode='bilinear'),
-            features[8],
+            F.interpolate(features[6], size=self.high_feature_size, mode='bilinear'),
+            features[7],
         ]
         high_features = torch.cat(high_features, dim=1)
         high_features = self.conv_high_features(high_features)
